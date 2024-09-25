9bd68658d663944304aa663386dcd9524838cc48
Trent <aple83709@gmail.com>
2024-09-25 17:53:53 +0800

    chore:添加meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Jackson產業觀點',
+  description: 'Jackson產業觀點',
+  verification: {
+    google: 'iP5orFFmfLOtuNnWzNO9cpucZkJalvFhbmiwpfIE2Jc',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh-Hant">
       <body>
         <Header />
         {children}
