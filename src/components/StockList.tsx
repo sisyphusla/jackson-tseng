b4eed677786723ed6c5a9b807c09f3cf5c71dba9
Trent <aple83709@gmail.com>
2024-10-09 16:17:59 +0800

    chore:修正vercel錯誤

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BaseStockData } from '@/types/stock';
 import { StockCard } from '@/components/StockCard';
 
