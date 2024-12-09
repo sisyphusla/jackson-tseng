6cf1064fd4661ae0a36c0f13033a1414cfc7ff07
Trent <aple83709@gmail.com>
2024-12-09 22:06:12 +0800

    fix:Missing Suspense boundary with useSearchParams

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/Footer';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Monitoring } from 'react-scan/dist/core/monitor/params/next';
-
+import { Suspense } from 'react';
 export const metadata: Metadata = {
   title: 'Jackson產業觀點',
   description: 'Jackson產業觀點',
@@ -20,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <body>
-        <Monitoring
-          apiKey={process.env.MONITORING_API_KEY || ''}
-          url="https://monitoring.react-scan.com/api/v1/ingest"
-        />
+        <Suspense fallback={null}>
+          <Monitoring
+            apiKey={process.env.REACT_SCAN_API_KEY || ''}
+            url="https://monitoring.react-scan.com/api/v1/ingest"
+          />
+        </Suspense>
         <Header />
         {children}
         <Footer />
