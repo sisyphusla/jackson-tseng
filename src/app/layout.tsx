dd69d0068eb77a6279abe801df56b89f8f8d6ee1
Trent <aple83709@gmail.com>
2024-12-09 21:48:38 +0800

    feat:add react-scan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
 import type { Metadata } from 'next';
+import { Monitoring } from 'react-scan/dist/core/monitor/params/next';
 
 export const metadata: Metadata = {
   title: 'Jackson產業觀點',
@@ -19,6 +20,10 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant">
       <body>
+        <Monitoring
+          apiKey={process.env.MONITORING_API_KEY || ''}
+          url="https://monitoring.react-scan.com/api/v1/ingest"
+        />
         <Header />
         {children}
         <Footer />
